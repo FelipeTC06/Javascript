8382830e63d7a6c24fed6eac09f7c3afa5523ee3
Felipe Torres Cresto <97850634+FelipeTC06@users.noreply.github.com>
2022-01-24 20:29:20 -0300

    condicoes

---
+++ exercicios/aula012/ex011.js
@@ -0,0 +1,9 @@
+var idade = 15
+if(idade < 16){
+    console.log('Não vota')
+}else if(idade => 18){
+        console.log('Voto obrigatório')
+    }else{
+        console.log('Voto opicional')
+    }
+
