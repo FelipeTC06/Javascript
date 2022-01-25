98a9da50bf891bbcb8b3e97f798e89a505fb7576
Felipe Torres Cresto <97850634+FelipeTC06@users.noreply.github.com>
2022-01-25 19:32:07 -0300

    repeticoes

---
+++ exercicios/aula013/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+do{
+    console.log(`${c}`)
+    c++
+}while(c < 7)
+
+
+/*
+while(c < 7 ){
+    console.log(`${c}`)
+    c++
+}
+*/
---
+++ exercicios/aula014/ambiente.js
@@ -0,0 +1,3 @@
+for(c=1;c < 7; c++){
+    console.log(`${c}`)
+11}
